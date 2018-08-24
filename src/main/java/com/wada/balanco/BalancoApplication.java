e092910e1fe5af20dd5e9068c6addc86a564a1d2
Leonardo Wada <leonardowada@hotmail.com>
2018-08-24 00:14:01 -0300

    servidor subindo

---
+++ src/main/java/com/wada/balanco/BalancoApplication.java
@@ -0,0 +1,17 @@
+package com.wada.balanco;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BalancoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BalancoApplication.class, args);
+
+
+
+	}
+
+
+}
---
+++ src/test/java/com/wada/balanco/BalancoApplicationTests.java
@@ -0,0 +1,18 @@
+package com.wada.balanco;
+
+import jdk.nashorn.internal.ir.annotations.Ignore;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@Ignore
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class BalancoApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
